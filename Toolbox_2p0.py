08cc1e0e9f4f865a0eef3dc277e171863ae39609
natamashii <126668878+natamashii@users.noreply.github.com>
2024-11-14 18:24:39 +0100

    Ich leide auch an großen seelischen Schmerzen

--- Toolbox_2p0.py
+++ Toolbox_2p0.py
@@ -151,10 +151,96 @@ def extract_version2(valid_data, all_dot_sizes, all_windows, frame_times):
             break_phases.append([current_phase_name, start_time, start_idx, end_time, end_idx])
     return time_points, phase_names, indices, break_phases
 
+# function to get sorting indices for cells in their best response to stimuli
+def get_sort_cells(indices, chosen_cells, num_conds, conds_dotsizes, conds_windows, conds_elevations):
+    # pre allocation
+    conditions = []
+    for_sorting = np.zeros((np.shape(chosen_cells)[0], 3, num_conds))
+    time_sorting = np.full((num_conds, 2, 3), np.nan)
+    split_counter = 0
+    # iterate over dot sizes
+    for ds in range(np.shape(indices)[0]):
+        # iterate over windows
+        for wind in range(np.shape(indices)[1]):
+            # iterate over elevations
+            for el in range(np.shape(indices)[2]):
+                # only continue if repetition exists
+                if not np.isnan(indices[ds, wind, el, :, :]).any():
+                    # iterate over repetitions
+                    # set condition as string
+                    conditions.append(f"{conds_dotsizes[ds]}_{conds_windows[wind]}_{conds_elevations[ds][el]}")
+                    for rep in range(np.shape(indices)[3]):
+                        # find start & end
+                        cond_start = indices[ds, wind, el, rep, 0].astype("int64")
+                        time_sorting[split_counter, 0, rep] = cond_start
+                        cond_end = indices[ds, wind, el, rep, 2].astype("int64")
+                        time_sorting[split_counter, 1, rep] = cond_end
+                        # get average of cell's dff in this phase
+                        for cell, trace in enumerate(chosen_cells):
+                            avg = np.nanmean(trace[cond_start:cond_end])
+                            # average dff across repetition
+                            for_sorting[cell, rep, split_counter] = avg
+                    split_counter += 1
+    # peak of average dff over condition: 40 values per cell
+    for_sorting_el = np.nanmax(for_sorting, axis=1)
+    # arg peak of peak mean dff: indices of condition the cell correlates best to
+    cell_sorting = np.nanargmax(for_sorting_el, axis=1)
+    return cell_sorting, time_sorting, conditions
+
+# function to sort cells in their best response to stimuli
+def sort_cells(sorting, num_conds, chosen_cells, indices):
+    # pre allocation
+    sorted_cells = list([] * num_conds)
+    amount_cells = np.zeros((num_conds))
+    idx_cells = list([] * num_conds)
+
+    t_min, t_max = t_min_max(indices=indices)
+
+    # iterate over all possible conditions of the stimulus
+    for cond in range(num_conds):
+        # find how many cells were sorted to this condition
+        amount_cells[cond] = np.shape(np.where(sorting == cond))[1]
+        # get indices in sorting of cells sorted to this condition
+        ind_cells = np.where(sorting == cond)[0].astype("int64")
+        idx_cells.append(ind_cells)
+        # only continue if there are cells sorted to this condition
+        if amount_cells[cond] > 0:
+            # iterate over relevant cells
+            for cell_idx in ind_cells:
+                get_cell = chosen_cells[cell_idx, t_min:t_max]
+                sorted_cells.append(get_cell)
+    sorted_cells = np.array(sorted_cells)
+    return sorted_cells
+
+# function to sort time dimension into stimuli
+def sort_times(sorted_cells, time_sorting, t_min):
+    # pre allocation
+    time_sorted_cells = np.zeros((np.shape(sorted_cells)[0], 1))
+    time_counter = 0
+    # iterate over conditions
+    for cond in range(np.shape(time_sorting)[0]):
+        # iterate over repetitions of this condition
+        for rep in range(np.shape(time_sorting)[2]):
+            start = int(time_sorting[cond, 0, rep] - t_min)
+            end = int(time_sorting[cond, 1, rep] - t_min)
+            # get amount of frames for this repetition
+            interval = end - start
+            # append to pre allocated array
+            time_sorted_cells = np.concat((time_sorted_cells, sorted_cells[:, start:end]), axis=1)
+            time_counter += interval
+    # exclude first col (placeholder col)
+    time_sorted_cells = time_sorted_cells[:, 1:]
+    return time_sorted_cells
 
 # function to convert time point into calcium frame indices
 def convert_time_frame(frame_times, time_point_variable):
     return min(enumerate(frame_times), key=lambda x: abs(x[1] - time_point_variable))[0]
+
+# function to get t_min & t_max of Dot Stimulus Interval
+def t_min_max(indices):
+    t_min = np.nanmin(indices).astype("int64")
+    t_max = np.nanmax(indices).astype("int64")
+    return t_min, t_max
 
 # function to smooth data via sliding window
 def avg_smooth(data, window):
@@ -206,7 +292,6 @@ def calc_dff_wind(F, window, frame_rate=2.18):
         progress_counter = progress_counter + 1
     return dff_wind
 
-
 # function to calculate dff via global median
 def calc_dff(F):
     print("Calculate dff")
@@ -221,7 +306,6 @@ def calc_dff(F):
         progress_counter = progress_counter + 1
     return dff
 
-
 # function to calculate dff via z-score
 def calc_dff_zscore(F):
     print("Calculate dff with z-score")
@@ -234,6 +318,48 @@ def calc_dff_zscore(F):
         __printProgressBar(progress_counter, l, prefix="progress:", suffix="complete", length=50)
         progress_counter = progress_counter + 1
     return dff
+
+def get_mean_dff_trace(chosen_cells, indices,):
+    print("Compute average dFF curve over all 3 repetitions")
+    # pre allocation
+    best_cells_avg = []
+    # iterate over cells
+    indices_mean_dff = np.full((len(indices), len(indices[0]), len(indices[0,0]), 2), np.nan)
+    counter = 0
+    for cell in range(np.shape(chosen_cells)[0]):
+        # iterate over dot sizes
+        avg_dff_cell = []
+        for d in range(len(indices)):
+            # iterate over windows
+            for window in range(len(indices[d])):
+                # identify amount of elevation levels used for this stimulus
+                for elevation in range(len(indices[d, window])):
+                    if not np.isnan(indices[d, window, elevation]).any():
+                        counter += 1
+                        # identify phases of repetitions
+                        rep1 = chosen_cells[cell, int(indices[d, window, elevation, 0, 0]):
+                                         int(indices[d, window, elevation, 0, 2])]
+                        rep2 = chosen_cells[cell, int(indices[d, window, elevation, 1, 0]):
+                                         int(indices[d, window, elevation, 1, 2])]
+                        rep3 = chosen_cells[cell, int(indices[d, window, elevation, 2, 0]):
+                                         int(indices[d, window, elevation, 2, 2])]
+                        rep_matrix = np.full((3, max([len(rep1), len(rep2), len(rep3)])), np.nan)
+                        rep_matrix[0, 0 : len(rep1)] = rep1
+                        rep_matrix[1, 0 : len(rep2)] = rep2
+                        rep_matrix[2, 0 : len(rep3)] = rep3
+                        avg_dff = np.nanmean(rep_matrix, axis = 0)
+                        #get new start-indices
+                        indices_mean_dff[d, window, elevation, 0] = len(avg_dff_cell)
+                        #extend average dff values
+                        avg_dff_cell.extend(avg_dff)
+                        #get new end-indices
+                        indices_mean_dff[d, window, elevation, 1] = len(avg_dff_cell)
+        best_cells_avg.append(avg_dff_cell)
+    counter /= np.shape(chosen_cells)[0]
+    mean_dff_best_cells = np.zeros((len(best_cells_avg), len(best_cells_avg[0])))
+    for cell in range(np.shape(mean_dff_best_cells)[0]):
+        mean_dff_best_cells[cell, :] = best_cells_avg[cell]
+    return mean_dff_best_cells, indices_mean_dff
 
 # function of regressor curve (for convolution), Credits to Carina
 def CIRF(regressor, n_ca_frames, tau=1.6):
@@ -370,48 +496,39 @@ def compare(corr_cells, autocorr_cells):
 def convert_time_frame(frame_times, time_point_variable):
     return min(enumerate(frame_times), key=lambda x: abs(x[1] - time_point_variable))[0]
 
-def get_mean_dff_trace(chosen_cells, indices,):
-    print("Compute average dFF curve over all 3 repetitions")
+# function to compute z score of chosen cells
+def z_score_comp(chosen_cells, break_phases, tail_length=5):
+    print("Compute Z Score of Chosen Cells")
     # pre allocation
-    best_cells_avg = []
-    # iterate over cells
-    indices_mean_dff = np.full((len(indices), len(indices[0]), len(indices[0,0]), 2), np.nan)
-    counter = 0
-    for cell in range(np.shape(chosen_cells)[0]):
-        # iterate over dot sizes
-        avg_dff_cell = []
-        for d in range(len(indices)):
-            # iterate over windows
-            for window in range(len(indices[d])):
-                # identify amount of elevation levels used for this stimulus
-                for elevation in range(len(indices[d, window])):             
-                    if not np.isnan(indices[d, window, elevation]).any():
-                        counter += 1 
-                        # identify phases of repetitions
-                        rep1 = chosen_cells[cell, int(indices[d, window, elevation, 0, 0]):
-                                         int(indices[d, window, elevation, 0, 2])]
-                        rep2 = chosen_cells[cell, int(indices[d, window, elevation, 1, 0]):
-                                         int(indices[d, window, elevation, 1, 2])]
-                        rep3 = chosen_cells[cell, int(indices[d, window, elevation, 2, 0]):
-                                         int(indices[d, window, elevation, 2, 2])]
-                        rep_matrix = np.full((3, max([len(rep1), len(rep2), len(rep3)])), np.nan)
-                        rep_matrix[0, 0 : len(rep1)] = rep1
-                        rep_matrix[1, 0 : len(rep2)] = rep2
-                        rep_matrix[2, 0 : len(rep3)] = rep3
-                        avg_dff = np.nanmean(rep_matrix, axis = 0)
-                        #get new start-indices
-                        indices_mean_dff[d, window, elevation, 0] = len(avg_dff_cell)
-                        #extend average dff values
-                        avg_dff_cell.extend(avg_dff)                 
-                        #get new end-indices
-                        indices_mean_dff[d, window, elevation, 1] = len(avg_dff_cell)                 
-        best_cells_avg.append(avg_dff_cell)
-    counter /= np.shape(chosen_cells)[0]
-    mean_dff_best_cells = np.zeros((len(best_cells_avg), len(best_cells_avg[0])))
-    for cell in range(np.shape(mean_dff_best_cells)[0]):
-        mean_dff_best_cells[cell, :] = best_cells_avg[cell]
-    return mean_dff_best_cells, indices_mean_dff
+    mean_std = np.zeros((2, np.shape(chosen_cells)[0]))
+    cell_breaks = []
+    z_score_cells = np.zeros(np.shape(chosen_cells))
+    break_start = np.zeros((len(break_phases)))
+    break_end = np.zeros((len(break_phases)))
+    # iterate over all cells
+    for cell, trace in enumerate(chosen_cells):
+        # get pauses of each cell
+        current_cell_break = []
+        # iterate over all break phases
+        for b_phase in range(len(break_phases)):
+            start_ind = break_phases[b_phase][2]
+            break_start[b_phase] = start_ind
+            end_ind = break_phases[b_phase][4]
+            break_end[b_phase] = end_ind
+            current_cell_break.extend(chosen_cells[cell, start_ind + tail_length:end_ind])
+        # save all break dff frames for current cell
+        cell_breaks.append(current_cell_break)
+        # get mean for this cell
+        cell_mean = np.mean(current_cell_break)
+        mean_std[0, cell] = cell_mean
+        # get std for this cell
+        cell_std = np.std(current_cell_break)
+        mean_std[1, cell] = cell_std
 
+        # get z score for this cell
+        for frame_idx, frame in enumerate(trace):
+            z_score_cells[cell, frame_idx] = (frame - cell_mean) / cell_std
+    return z_score_cells, mean_std
 
 # function to make plots more aesthetic
 def plot_beautiful(ax, xmin=None, xmax=None, ymin=None, ymax=None, step=None,
@@ -428,6 +545,16 @@ def plot_beautiful(ax, xmin=None, xmax=None, ymin=None, ymax=None, step=None,
         # move legend to right & outside of plot
         ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., markerscale=5)
 
+# function for pixel plot
+def pixel_plot(time_sorted_cells, cbar_label, title, cmap="PiYG"):
+    fig, axs = plt.subplots()
+    pixelplot = axs.imshow(time_sorted_cells, cmap=cmap)
+    # Adjust Plot
+    axs.set_xlabel("Time [s]")
+    axs.set_ylabel("Cells")
+    axs.set_title(title)
+    fig.colorbar(pixelplot)
+    return fig
 
 # Function for Progressbar, Credits to David
 def __printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█',
